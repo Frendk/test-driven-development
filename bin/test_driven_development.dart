f37005600712842b242cd87c1c5fea9d01d3a57f
Frendk <ayushazhamsaranov@gmail.com>
2025-12-11 09:55:27 +0800

    Initial TDD project setup

---
+++ bin/test_driven_development.dart
@@ -0,0 +1,5 @@
+import 'package:test_driven_development/test_driven_development.dart' as test_driven_development;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_driven_development.calculate()}!');
+}
---
+++ test/test_driven_development_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_driven_development/test_driven_development.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
